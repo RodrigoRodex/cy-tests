66764be2e6dce666b23a36bdef6c9f8b0b82435e
Rodrigo <rodex97@gmail.com>
2024-07-05 18:56:23 -0300

    Testes do carrosel de slider

--- cypress/e2e/automation_tests.cy.js
+++ cypress/e2e/automation_tests.cy.js
@@ -4,7 +4,28 @@ describe('first test in the Automation Exercise', () =>{
   })
 
   it('it test the slider-carousel', () =>{
-    cy.get("carousel slide")
-    // cy.get("carousel-indicators")
+    // get the class slider-carousel and find the last a, then click at it
+    const indicator = cy.get("#slider-carousel").find("ol").get('li').eq(9)
+    cy.get("#slider-carousel")
+      .find('a')
+      .last()
+      .click()
+      // asserts that the carousel indicator changed
+      cy.get("#slider-carousel")
+        .find("ol")
+        .get('li')
+        .eq(9)
+        .should('have.class', 'active')
+    // #
+  })
+  // get the buttons in the slider carousel and test both of them
+  it('tests the buttons in the slider carousel', () =>{
+    cy.get("#slider-carousel")
+      .get(".carousel-inner")
+      .find("a").eq(0).click()
+      cy.visit("https://www.automationexercise.com/")
+      cy.get("#slider-carousel")
+      .get(".carousel-inner")
+      .find("a").eq(1).click()
   })
 })
